a7966db25a55e31d20b84c8e6fb3abd609ecfa36
Youen Chéné <youen.chene@gadz.org>
2025-01-19 15:27:08 +0100

    feat: new export for ACE pTileData (UBYTE)

--- gogitum.mjs
+++ gogitum.mjs
@@ -11,8 +11,8 @@ function swap16(val) {
            | ((val >> 8) & 0xFF);
 }
 
-var customMapFormat = {
-    name: "GOGITUM Export",
+var customMapFormat8 = {
+    name: "GOGITUM Export 8bits tiled and size",
     extension: "dat",
 
     write: function(map, fileName) {
@@ -74,8 +74,80 @@ var customMapFormat = {
 }
 
 
+var customMapFormat816 = {
+    name: "GOGITUM Export 8 bits tiled 16 bits size",
+    extension: "dat",
+
+    write: function(map, fileName) {
+
+		var w = map.width;
+		var h = map.height;
+        
+		var arrayBuffer = new ArrayBuffer((4+(w*h)))
+	
+
+		var dataView = new DataView(arrayBuffer);
+		var width = w-1;
+		dataView.setUint16(0, width, false);
+		var height = h-1;
+		dataView.setUint16(2, height, false);
+
+
+		var byteBuffer = new Int8Array(arrayBuffer)
+		
+		for (var i = 0; i < map.layerCount; ++i) {
+            
+			var layer = map.layerAt(i);
+			
+			if (layer.isTileLayer && layer.name == "Terrain") {
+				
+				var idx=4;
+				
+				for (var y = 0; y < h; ++y) {
+					for (var x = 0; x < w; ++x) {
+						var tile = layer.tileAt(x, y);
+						if (tile != null) {
+							byteBuffer[idx]=tile.id+1;
+						} else {
+							byteBuffer[idx]=0;
+						}
+						idx++;
+					}
+				}
+
+			}
+			
+			if (layer.isTileLayer && layer.name == "Structures") {
+				
+				var idx=4;
+				
+				for (var y = 0; y < h; ++y) {
+					for (var x = 0; x < w; ++x) {
+						var tile = layer.tileAt(x, y);
+						if (tile != null) {
+							byteBuffer[idx]=tile.id+1;
+						}
+						idx++;
+					}
+				}
+
+			}			
+			
+        }
+
+
+
+		var file = new BinaryFile(fileName, BinaryFile.WriteOnly);
+		file.write(arrayBuffer);
+		file.commit();
+		
+
+    },
+}
+
+
 var customMapFormat16 = {
-    name: "GOGITUM 16 Export",
+    name: "GOGITUM Export 16bits tiled and size",
     extension: "dat",
 
     write: function(map, fileName) {
@@ -96,7 +168,7 @@ var customMapFormat16 = {
 			
 			if (layer.isTileLayer && layer.name == "Terrain") {
 				
-				var idx=2;
+				var idx=4;
 				
 				for (var y = 0; y < h; ++y) {
 					for (var x = 0; x < w; ++x) {
@@ -138,6 +210,73 @@ var customMapFormat16 = {
 			
         } 
 
+		var file = new BinaryFile(fileName, BinaryFile.WriteOnly);
+		file.write(arrayBuffer);
+		file.commit();
+		
+    },
+}
+
+var customMapFormatACE816 = {
+    name: "GOGITUM Export ACE - pTileData (UBYTE)",
+    extension: "dat",
+
+    write: function(map, fileName) {
+
+		var w = map.width;
+		var h = map.height;
+        
+		var arrayBuffer = new ArrayBuffer((4+(w*h)))
+	
+
+		var dataView = new DataView(arrayBuffer);
+		var width = w-1;
+		dataView.setUint16(0, width, false);
+		var height = h-1;
+		dataView.setUint16(2, height, false);
+
+
+		var byteBuffer = new Int8Array(arrayBuffer)
+		
+		for (var i = 0; i < map.layerCount; ++i) {
+            
+			var layer = map.layerAt(i);
+			
+			if (layer.isTileLayer && layer.name == "Terrain") {
+				
+				var idx=4;
+				for (var x = 0; x < w; ++x) {
+					for (var y = 0; y < h; ++y) {
+						var tile = layer.tileAt(x, y);
+						if (tile != null) {
+							byteBuffer[idx]=tile.id;
+						} else {
+							byteBuffer[idx]=0;
+						}
+						idx++;
+					}
+				}
+
+			}
+			
+			if (layer.isTileLayer && layer.name == "Structures") {
+				
+				var idx=4;
+				
+				for (var x = 0; x < w; ++x) {
+					for (var y = 0; y < h; ++y) {
+						var tile = layer.tileAt(x, y);
+						if (tile != null) {
+							byteBuffer[idx]=tile.id;
+						}
+						idx++;
+					}
+				}
+
+			}			
+			
+        }
+
 
 
 		var file = new BinaryFile(fileName, BinaryFile.WriteOnly);
@@ -148,6 +287,10 @@ var customMapFormat16 = {
     },
 }
 
-tiled.registerMapFormat("custom8", customMapFormat)
+tiled.registerMapFormat("custom8", customMapFormat8)
+
+tiled.registerMapFormat("custom816", customMapFormat816)
 
 tiled.registerMapFormat("custom16", customMapFormat16)
+
+tiled.registerMapFormat("custom816AXE", customMapFormatACE816)
